275707fb7025614bed67ba4c62d0acfa042179e8
Yura <yurii.st11@gmail.com>
2025-06-20 17:21:38 +0300

    make build

---
+++ index.3e6b5b1f.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul"),a=!1;for(var l in t){var c=document.createElement("li");c.textContent=l,Object.keys(t[l]).length>0&&e(c,t[l]),r.appendChild(c),a=!0}a&&n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.3e6b5b1f.js.map
